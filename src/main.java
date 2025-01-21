a07716d4f38f7143c9a98c067337093adf686e7d
Natalia Kashcheeva <n.kashcheeva96@gmail.com>
2025-01-21 21:16:14 +0300

    fix: main.java

--- src/main.java
+++ src/main.java
@@ -1,2 +1,25 @@
 public class main {
+    public static void main(String[] args) {
+
+
+        int balance = 50;
+        int score = 1001;
+
+
+        int bonus = (score / 100);
+        if (score >= 1000) {
+            int result = (balance + score) + bonus;
+
+            System.out.println("Итоговый счет" + result);
+        } else {
+            if (score >= 999) ;
+            {
+
+                System.out.println("Итоговый счет" + (balance + score));
+            }
+        }
+    }
+
+
 }
+
