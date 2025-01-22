1863db7f84d8cb13d8c81ac511a90d49146dacca
Natalia Kashcheeva <n.kashcheeva96@gmail.com>
2025-01-22 18:06:17 +0300

    fixnew: main.java

--- src/main.java
+++ src/main.java
@@ -1,25 +1,24 @@
 public class main {
     public static void main(String[] args) {
-
-
         int balance = 50;
-        int score = 1001;
+        int score = 8500;
+        int bonus = 0; 
 
+        // Определяем, сколько бонусных рублей получит клиент
+        if (score >= 1001) {
+            bonus = score / 100; // Расчёт бонусных рублей
 
-        int bonus = (score / 100);
-        if (score >= 1000) {
-            int result = (balance + score) + bonus;
-
-            System.out.println("Итоговый счет" + result);
+            balance += score + bonus;
         } else {
-            if (score >= 999) ;
-            {
+            balance += score; // Если сумма пополнения меньше 1001, просто добавляем score к балансу
+        }
 
-                System.out.println("Итоговый счет" + (balance + score));
-            }
+        System.out.println("Итоговый счёт: " + balance);
+
+        if (bonus > 0) {
+            System.out.println("Бонусные рубли: " + bonus); // Проверяем, есть ли бонусные рубли
+        } else {
+            System.out.println("Бонусных рублей нет");
         }
     }
-
-
 }
-
